0ea199e65e742b93690d3d4c19ae8e9383bf9543
Chris Wander <chriswandermail@gmail.com>
2021-01-24 17:39:06 -0500

    added package

--- src/views/Landing/Landing.jsx
+++ src/views/Landing/Landing.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Typist from 'react-typist';
 
 import './Landing.scss';
 
 const Landing = React.memo(() => {
 	return (
 		<div className="landing">
-			<h2 className="landing-title">Hello</h2>
-			<h2 className="landing-subtitle">chris here </h2>
+			<Typist>
+				<Typist.Delay count={6} ms={500} />
+				<h2 className="landing-title">Hello</h2>
+				<h2 className="landing-subtitle">chris here </h2>
+				{/* <Typist.Delay count={2} ms={500} /> */}
+			</Typist>
 			<ul className="landing-text-list">
 				<li className="text-list-li">
 					<i className="fas fa-terminal"></i> full stack developer
